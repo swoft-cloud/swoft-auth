fa6d10aadc54f7fe373d99a3cf7a3dca4171f920
ott321 <ott321@yeah.net>
2018-05-21 21:45:16 +0800

    add test

---
+++ test/Cases/AuthManagerTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace SwoftTest\Auth;
+
+/**
+ * Class AuthManagerTest
+ * @package SwoftTest\Auth
+ */
+class AuthManagerTest extends AbstractTestCase
+{
+}
---
+++ test/Cases/AuthUserServiceTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  group@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+
+namespace SwoftTest\Auth;
+
+class AuthUserServiceTest extends AbstractTestCase
+{
+    /**
+     * @test
+     * @covers AuthUserService::getSession()
+     */
+    public function testGetSession()
+    {
+    }
+}
