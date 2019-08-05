ce462ff0708e79aad058400e9080b38bd47ddd83
inhere <in.798@qq.com>
2019-08-05 21:30:04 +0800

    update some

--- src/Parser/JWTTokenParser.php
+++ src/Parser/JWTTokenParser.php
@@ -29,13 +29,13 @@ class JWTTokenParser implements TokenParserInterface
     public const ALGORITHM_RS256 = 'RS256';
 
     /**
-     * Value("${config.auth.jwt.algorithm}")
+     * Value("auth.jwt.algorithm")
      * @var string
      */
     protected $algorithm = self::ALGORITHM_HS256;
 
     /**
-     * Value("${config.auth.jwt.secret}")
+     * Value("auth.jwt.secret")
      * @var string
      */
     protected $secret = 'swoft';
